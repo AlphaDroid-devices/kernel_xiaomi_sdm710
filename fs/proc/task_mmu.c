3a4f298e712290fbc430eb1e17100cb5ac8b4072
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-01-27 21:01:00 +0800

    fs: susfs: change inode->i_mapping->flags to inode->i_state for storing AS_FLAGS_ bit
    Some of the susfs features are working temporarily and did not persist after the command was executed.
    Such as when opening the app then close it and wait for few seconds to few mins then the detection came back.
    
    This might be due to storing SUSFS 'AS_FLAGS_' to inode->i_mapping->flags may not be working on non-gki.
    On SUSFS v1.5.5 - v1.5.9, inode->i_state is used to store 'AS_FLAGS_' on non-gki kernels.
    
    So this commit reverts it to use inode->i_state to store 'AS_FLAGS_' on most of the susfs features.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -373,7 +373,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
-		if (unlikely(inode->i_mapping->flags & BIT_SUS_MAPS) && susfs_is_current_proc_umounted()) {
+		if (unlikely(inode->i_state & BIT_SUS_MAPS) && susfs_is_current_proc_umounted()) {
 			seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
 			seq_put_hex_ll(m, NULL, vma->vm_start, 8);
 			seq_put_hex_ll(m, "-", vma->vm_end, 8);
@@ -391,7 +391,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 		}
 #endif
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-		if (unlikely(inode->i_mapping->flags & BIT_SUS_KSTAT)) {
+		if (unlikely(inode->i_state & BIT_SUS_KSTAT)) {
 			susfs_sus_ino_for_show_map_vma(inode->i_ino, &dev, &ino);
 			goto bypass_orig_flow;
 		}
@@ -954,9 +954,6 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 		show_smap_vma_flags(m, vma);
 	}
 
-#ifdef CONFIG_KSU_SUSFS_SUS_MAP
-bypass_orig_flow:
-#endif
 	m_cache_vma(m, vma);
 	return ret;
 }
@@ -1626,7 +1623,7 @@ static ssize_t pagemap_read(struct file *file, char __user *buf,
 		vma = find_vma(mm, start_vaddr);
 		if (vma && vma->vm_file) {
 			struct inode *inode = file_inode(vma->vm_file);
-			if (unlikely(inode->i_mapping->flags & BIT_SUS_MAPS) && susfs_is_current_proc_umounted()) {
+			if (unlikely(inode->i_state & BIT_SUS_MAPS) && susfs_is_current_proc_umounted()) {
 				pm.show_pfn = false;
 				pm.buffer->pme = 0;
 			}
